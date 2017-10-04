c6c2df8cbdb59b3404d187424093d40ec735bfd9
James Fonzone <jfonzone@outlook.com>
2017-10-04 17:01:46 -0400

    Splash Screen

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class LevelManager : MonoBehaviour {
+	private float waitTime = 4.0f;
+	
+	void Update(){
+		if (Time.realtimeSinceStartup >= waitTime){
+			LoadLevel("Start Menu");
+		}		
+	}
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+}
