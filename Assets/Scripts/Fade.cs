d63738b74b406d1a58d7d88dbdf2c4298bce6549
James Fonzone <jfonzone@outlook.com>
2017-10-12 20:28:09 -0400

    fade in title screen

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour {
+	public float fadeTime = 3.0f;
+	
+	private Image fadePanel;
+	private Color currentColor = Color.black;
+
+	// Use this for initialization
+	void Start () {
+		fadePanel = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FadeIn();
+	}
+	
+	void FadeIn(){
+		if (Time.timeSinceLevelLoad < fadeTime){
+			float alphaChangePerFrame = Time.deltaTime / fadeTime;
+			currentColor.a -= alphaChangePerFrame;
+			fadePanel.color = currentColor;
+		}
+		else {
+			//deactivate object once fade is finished
+			gameObject.SetActive(false);
+		}
+	}
+}
