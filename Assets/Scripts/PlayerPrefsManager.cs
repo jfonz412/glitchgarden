49758e71551a41984218af7ec81591420bc9eca9
James Fonzone <jfonzone@outlook.com>
2017-10-14 23:51:05 -0400

    add PlayerPrefsManager

--- Assets/Scripts/PlayerPrefsManager.cs
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -3,13 +3,57 @@ using System.Collections;
 
 public class PlayerPrefsManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	const string MASTER_VOLUME_KEY = "master_volume";
+	const string DIFFICULTY_KEY = "difficulty";
+	const string LEVEL_KEY = "level_unlocked_";
 	
+	//Set a float via Unity's Player Prefs
+	public static void SetMasterVolume(float volume){
+
+		if (volume >= 0f && volume <= 1f){
+			PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+		}else {
+			Debug.LogError("Invalid volume input");
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	//Return a float via Unity's Player Prefs
+	public static float GetMasterVolume(){
+		return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
 	}
+	
+	public static void UnlockLevel(int level){
+		if (level <= Application.levelCount - 1){
+			//sets string to LEVEL_KEY2, the '1' == true
+			PlayerPrefs.SetInt (LEVEL_KEY + level.ToString (), 1);
+		}else{
+			Debug.LogError("Invalid level input (UnlockLevel)");
+		}
+	}
+	
+	//here we check to see if the level we pass in returns 1 or null
+	public static bool IsLevelUnlocked(int level){
+		int levelValue = PlayerPrefs.GetInt(LEVEL_KEY + level.ToString());
+		bool isLevelUnlocked = (levelValue == 1); //will be true if levelValue == 1
+		
+		if (level <= Application.levelCount - 1){
+			return isLevelUnlocked;
+		}else{
+			Debug.LogError("Invalid level input (IsLevelUnlocked)");
+			return false;
+		}
+	}
+	
+	public static void SetDifficultyLevel(float difficultyLevel){
+		if (difficultyLevel >= 0f && difficultyLevel <= 1f){
+			PlayerPrefs.SetFloat(DIFFICULTY_KEY, difficultyLevel);
+		}else{
+			Debug.LogError("Invalid difficulty setting");
+		}
+	}
+	
+	public static float GetDifficultyLevel(){
+		return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
+	}
+	
 }
---
+++ Assets/Scripts/PrefsTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefsTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//test master volume
+		PlayerPrefsManager.SetMasterVolume(0.5f);
+		print ("what is the master volume?");
+		Debug.Log(PlayerPrefsManager.GetMasterVolume());
+	
+		//test level unlock (will be unlocked until again locked)
+		Debug.Log (PlayerPrefsManager.IsLevelUnlocked(1));
+		Debug.Log (PlayerPrefsManager.IsLevelUnlocked(2));
+		PlayerPrefsManager.UnlockLevel(1);
+		PlayerPrefsManager.UnlockLevel(2);
+		print ("is level 1 unlocked?");
+		Debug.Log (PlayerPrefsManager.IsLevelUnlocked(1));
+		print ("is level 2 unlocked?");
+		Debug.Log (PlayerPrefsManager.IsLevelUnlocked(2));
+		
+		PlayerPrefsManager.SetDifficultyLevel(1.8f);
+		print ("what is the difficulty level?");
+		Debug.Log(PlayerPrefsManager.GetDifficultyLevel());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
