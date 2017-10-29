6fa855fa940677e9f1da981996af9a93ffa3d65a
James Fonzone <jfonzone@outlook.com>
2017-10-29 15:59:57 -0400

    projectiles deal damage

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -14,4 +14,16 @@ public class Projectile : MonoBehaviour {
 	void Update () {
 		transform.Translate(Vector3.right * speed * Time.deltaTime);
 	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		GameObject obj = other.gameObject;
+
+		if (!obj.GetComponent<Attacker>()){
+			return;
+		}
+		Health enemyHealth = obj.GetComponent<Health>();
+		
+		enemyHealth.DealDamage(damage);
+		Destroy(gameObject);	
+	}
 }
