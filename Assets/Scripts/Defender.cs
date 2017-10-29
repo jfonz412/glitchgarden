a45edff1dcb09f191b970e88ebfe8ab722b8e26f
James Fonzone <jfonzone@outlook.com>
2017-10-29 13:22:13 -0400

    projectiles fire from defenders

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -3,22 +3,4 @@ using System.Collections;
 
 public class Defender : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		if(gameObject.tag == "Projectile"){
-			Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D>();
-			myRigidBody.isKinematic = true;
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
-	void OnTriggerEnter2D(Collider2D other){
-		if(other.gameObject.tag == "Attacker"){
-			//Debug.Log ("Defender under attack!");
-		}
-	}
 }
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+	public GameObject projectile, projectileParent, gun;
+	
+	//animator can reach into private methods
+	private void Fire(){
+		GameObject newProjectile = Instantiate(projectile) as GameObject;
+		newProjectile.transform.SetParent(projectileParent.transform);
+		newProjectile.transform.position = gun.transform.position;	
+	}
+}
