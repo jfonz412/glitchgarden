499b3e51e18bd7f02ca0133b184a2e4fbff48d3f
James Fonzone <jfonzone@outlook.com>
2017-10-28 21:00:08 -0400

    deal damage, destroy, resume walking

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -3,28 +3,43 @@ using System.Collections;
 
 public class Attacker : MonoBehaviour {
  	private GameObject currentTarget;
+	private Health targetHealth;
+	private Animator animator;
 	private float currentSpeed;
 	
 	// Use this for initialization
 	void Start () {
 		Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D>();
 		myRigidBody.isKinematic = true;
+		
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
+		if(!currentTarget){
+			animator.SetBool("isAttacking", false);
+		}
 	}
 	
 	public void SetSpeed(float speed){
 		currentSpeed = speed;
 	}
 	
+	//When attacker isAttacking, set target to damage
 	public void Attack(GameObject obj){
 		currentTarget = obj;
 	}
 	
+	//actually talks to target's health to deal damage. called from animator
 	public void StrikeCurrentTarget(float damage){
-		Debug.Log(gameObject.name + " has dealt " + damage + " damage!");
+		//Debug.Log(gameObject.name + " has dealt " + damage + " damage to the " + currentTarget.name);
+		if(currentTarget){
+			Health health = currentTarget.GetComponent<Health>();
+			if(health){
+				health.DealDamage(damage);
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+	public float health;
+	
+	public void DealDamage(float damage){
+		health -= damage;
+		if (health <= 0){
+			//animate death, move method to animator
+			DestroyObject();
+		}
+	}
+	
+	//to be called from animator at the end of death animation
+	public void DestroyObject(){
+		Destroy(gameObject);
+	}
+}
